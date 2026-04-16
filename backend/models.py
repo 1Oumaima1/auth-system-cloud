83830d8f4e426e5a5e6219266a09c5d4571a4594
Oumaima Amlou <139626051+1Oumaima1@users.noreply.github.com>
2026-04-16 17:35:19 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -11,8 +11,12 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     google_id = Column(String, nullable=True, unique=True)
     password = Column(String, nullable=True)
-    role = Column(String, default="user")  # user or admin
+    role = Column(String, default="user")
     is_verified = Column(Boolean, default=False)
     verification_token = Column(String, nullable=True)
+    
+    # AJOUTE CES DEUX LIGNES ICI :
+    reset_token = Column(String, nullable=True)
+    reset_expires_at = Column(DateTime, nullable=True)
+    
     created_at = Column(DateTime, default=datetime.utcnow)
-
